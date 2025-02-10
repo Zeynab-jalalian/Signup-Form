4d7a450d97bb6119e78e77a4064fa0d97a517f0d
zeynab jalalian <zjalalianweb@gmail.com>
2025-02-10 14:17:45 +0330

    js2

--- script.js
+++ script.js
@@ -16,26 +16,59 @@ let current=1;
 firstNextBtn.addEventListener("click", function () {
     slidePage.style.marginLeft = "-25%";
     bullet[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
     progressCheck[current - 1].classList.add("active");
     current+=1;
 })
 
 nextBtnSec.addEventListener("click", function () {
     slidePage.style.marginLeft = "-50%";
+    bullet[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    current+=1;
 })
 nextBtnThird.addEventListener("click", function () {
     slidePage.style.marginLeft = "-75%";
+    bullet[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    current+=1;
 })
+submitBtn.addEventListener("click", function () {
+    bullet[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    current+=1;
+    setTimeout(function(){
+    alert("You're seccessfully Signed up");
+    location.reload();
+    },800)
+})
+
 
 
 
 prevBtnSec.addEventListener("click", function () {
     slidePage.style.marginLeft = "0%";
+
+      bullet[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    current-=1;
 })
 
 prevBtnThird.addEventListener("click", function () {
     slidePage.style.marginLeft = "-25%";
+      bullet[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    current-=1;
 })
 prevBtnFourth.addEventListener("click", function () {
     slidePage.style.marginLeft = "-50%";
+      bullet[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    current-=1;
 })
